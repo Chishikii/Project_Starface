c4c17effb2a2662c7c7b1782851d230b1f562b5a
Zwackelmann173 <57451356+Zwackelmann173@users.noreply.github.com>
2020-01-07 22:38:04 +0100

    SceneSwitcher eingebaut. Wird Start menü
    Scenen können mit 'g' und 'h' gewechselt werden.
    Scene 1 soll Startbildschirm werden.
    Das .obj für die Ebene des Bildes wird von QT nicht erkannt (im debugger werden 0 tries gelesen und somit kann der das .png nicht drauflegen)
    
    Außerdem habe ich noch die Anfänge für einen Wassershader gemacht der auf eine Plane gelegt werden soll (noch nicht mit code gefüllt)
    -Plane angelegt
    -.vert und .Frag angelegt (mit der farbe rot gefüllt um zu sehen das die Plane funktioniert)
    shader.qrc bearbeitet das die shader auch erkannt werden

--- myscene.cpp
+++ myscene.cpp
@@ -3,7 +3,6 @@
 #include "scenemanager.h"
 #include "screenrenderer.h"
 #include "mainwindow.h"
-
 #include "mousekeyboardcameracontroller.h"
 #include "charactercontroller.h"
 #include "trianglemesh.h"
@@ -19,30 +18,114 @@
 #include "physicaccessablecamera.h"
 #include "modeltransformation.h"
 #include "simplesphere.h"
+#include "Roboter.h"
+#include "sunlight.h"
 
 #include <QDebug>
 
 Node *initScene1();
+Node *initScene2();
+
+ScreenRenderer* sortedRenderer;
+ScreenRenderer* preOrderRenderer;
+unsigned int myScene1, myScene2;
+
+class SceneSwitcher: public Listener
+{
+    void keyboard(int key, int modifier)
+    {
+        if (key == 'g')
+        {
+            sortedRenderer->setScene(myScene1);
+        }
+        else if(key =='h')
+        {
+            sortedRenderer->setScene(myScene2);
+
+        }
+    }
+};
 
 void SceneManager::initScenes()
 {
-
+    /*SceneSwitcher* lSwitch = new SceneSwitcher();
     PhysicAccessableCamera* cam = new PhysicAccessableCamera();
     cam->setFarPlane(5000.f);
     RenderingContext* myContext = new RenderingContext(cam);
     unsigned int myContextNr = SceneManager::instance()->addContext(myContext);
-    unsigned int myScene = SceneManager::instance()->addScene(initScene1());
-    ScreenRenderer *myRenderer = new ScreenRenderer(myContextNr, myScene);
-    Q_UNUSED(myRenderer)
+    unsigned int myScene = SceneManager::instance()->addScene(initScene1());*/
+
+
+
+    SceneSwitcher* lSwitch = new SceneSwitcher();
+    PhysicAccessableCamera* cam = new PhysicAccessableCamera();
+    cam->setFarPlane(5000.f);
+    RenderingContext* myContext = new RenderingContext(cam);
+    unsigned int myContextNr = SceneManager::instance()->addContext(myContext);
+    myScene1 = SceneManager::instance()->addScene(initScene1());
+    myScene2 = SceneManager::instance()->addScene(initScene2());
+    sortedRenderer = new ScreenRenderer(myContextNr, myScene2);
+    Q_UNUSED(lSwitch);
+
+    //Vorsicht: Die Szene muss initialisiert sein, bevor das Fenster verändert wird (Fullscreen)
+    SceneManager::instance()->setActiveScene(myScene2);
+    SceneManager::instance()->setActiveContext(myContextNr);
+    //SceneManager::instance()->setFullScreen();
+
+
+    /*Q_UNUSED(myRenderer)
     //Vorsicht: Die Szene muss initialisiert sein, bevor das Fenster verändert wird (Fullscreen)
     SceneManager::instance()->setActiveScene(myScene);
     SceneManager::instance()->setActiveContext(myContextNr);
-    //SceneManager::instance()->setFullScreen();
+    //SceneManager::instance()->setFullScreen();*/
 }
 
 Node *initScene1()
 {
-    //Projectpath
+    //    Objekte anlegen
+
+    /*Plane *StartMenu = new Plane(100,100);
+    Texture *t = StartMenu->getProperty<Texture>();
+    t->loadPicture(":/modelTextures/fighter_texture.png");*/
+
+    //Drawable *StartMenu = new Drawable(new TriangleMesh(":/modelObjects/fighterRot.obj"));
+    Drawable *StartMenu = new Drawable(new TriangleMesh(":/modelObjects/MainMenu2.obj"));
+    StartMenu->setStaticGeometry(false);
+    //Texturen laden
+    Texture *t = StartMenu->getProperty<Texture>();
+    //t->loadPicture(":/modelTextures/fighter_texture.png");
+    t->loadPicture(":/modelTextures/StartMenu.png");
+
+
+    Transformation *MenuPos= new Transformation();
+
+    MenuPos->rotate(0.0,90.0,0.0,0.0);
+
+    //watersurface->translate(0.0,0.0,-50);
+    Node *root = new Node();
+    Node *StartMenuNode = new Node(StartMenu);
+    Node *MenuPosNode = new Node(MenuPos);
+
+    root->addChild(MenuPosNode);
+    MenuPosNode->addChild(StartMenuNode);
+
+
+
+
+
+
+            //Szenengraph aufbauen
+
+
+
+
+        return(root);
+}
+
+
+Node *initScene2()
+{
+     //Projectpath
     QString path(SRCDIR);
 
     int terrainSize = 200;
@@ -50,6 +133,21 @@ Node *initScene1()
     //Terrain
     Heightmap *map = new Heightmap(terrainSize,200,50);
     Terrain *terrain = new Terrain(map);
+
+
+    //Water
+    /*Plane *water = new Plane(100,100);
+
+    Transformation *watersurface= new Transformation();
+    Shader* w = ShaderManager::getShader(path + QString("/shader/water.vert"), path + QString("/shader/water.frag"));
+
+    watersurface->rotate(0.0,90.0,0.0,0.0);
+    //watersurface->translate(0.0,0.0,-50);
+    water->setShader(w);
+
+    Node *waterNode = new Node(water);
+    Node *watersurfaceNode = new Node(watersurface);*/
+
 
     //shader
     Shader* s = ShaderManager::getShader(path + QString("/shader/texture.vert"), path + QString("/shader/texture.frag"));
@@ -85,6 +183,7 @@ Node *initScene1()
     //Texturen laden
     Texture *t = v_fighterModel->getProperty<Texture>();
     t->loadPicture(":/modelTextures/fighter_texture.png");
+
     v_fighterModel->setShader(s);
 
     ModelTransformation *v_fighterTransformation = v_fighterModel->getProperty<ModelTransformation>();
@@ -92,6 +191,7 @@ Node *initScene1()
     v_fighterTransformation->scale(QVector3D(0.1,0.1,0.1));
 
     root->addChild(new Node(v_fighterModel));
+
 
     DynamicCharacterWithCam *v_Character = v_PhysicEngine->createNewDynamicCharacterWithCam(v_fighterModel);
     v_Character->setCam(dynamic_cast<PhysicAccessableCamera*>(SceneManager::instance()->getActiveContext()->getCamera()));
@@ -125,12 +225,14 @@ Node *initScene1()
     terrainModel->setShader(b);
     //terrainModel->setShader(s);
 
-
     //posTerrain->translate(-terrainSize*10/2,-100,-terrainSize*10);
     //posTerrain->rotate(-20,QVector3D(1,0,0));
 
     // Baum aufbauen
+
     root->addChild(transformationTerrainNode);
+    /*root->addChild(watersurfaceNode);
+    watersurfaceNode->addChild(waterNode);*/
 
 
     return(root);
